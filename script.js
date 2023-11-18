da026fe2a663641f3e29865e3e6dd0a8324f39a4
Aleksandr Akob <aleksandrakob3@gmail.com>
2023-11-18 21:08:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// console.log('Привет, Александр!')
+// console.log("как дела")
+
+// --- ---
+// console.log(10 + 5)
+// console.log(10 - 3)
+
+// console.log(10 * 5)
+// console.log(12 / 2)
+
+// console.log(10 * (5 + 3))
+// --- ---
+
+let myName = 'Alex'
+
+console.log(myName)
+
+let youPhoto = "mother"
+
+console.log(youPhoto)
+
+console.log("Aleksandr Akob")
